37a6f5600048421d157ea4ce9a2f2843c3a3e28f
Tanmay Wankhede <110993855+tanmay-wankhede@users.noreply.github.com>
2023-01-03 12:41:38 +0530

    Delete restapi directory

--- restapi/models/__init__.py
+++
@@ -1,7 +0,0 @@
-#tables created by SQLAlchemy
-
-from models.item import ItemModel
-from models.tag import TagModel
-from models.store import StoreModel
-from models.item_tags import ItemsTags
-from models.user import UserModel
